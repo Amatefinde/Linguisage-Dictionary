6ede5fd9ffd7fef1f0d5d88d4aae2fc8083eae26
iRespectOnlyYen <MyNameIIIypuk@yandex.ru>
2023-11-29 23:14:09 +0300

    remove garbage

--- Parsers/collect_words_from_dictionary_list.py
+++ Parsers/collect_words_from_dictionary_list.py
@@ -1,7 +1,8 @@
 from typing import TypedDict
 import asyncio
 
-from config import URL_TO_WORD_LIST
+from core.config import settings
+from Dictionary import parse_page
 
 import aiohttp
 from bs4 import BeautifulSoup
@@ -14,8 +15,6 @@ class Word(TypedDict):
 
 headers = {
     "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/119.0.0.0 Safari/537.36 Edg/119.0.0.0"}
-
-LINK_TO_OXFORD_LIST = "https://www.oxfordlearnersdictionaries.com/wordlists/oxford3000-5000"
 
 
 async def fetch_data(session, url):
@@ -39,7 +38,10 @@ async def main(link_to_list: str):
     async with aiohttp.ClientSession(headers=headers) as session:
         row_html = await fetch_data(session, link_to_list)
         words: list[Word] = get_links_to_words(row_html)
-        print(words)
+        tasks = []
+        for word in words:  # type: Word
+            tasks.append(parse_page(session, word.get("link"), name=word.get("name")))
+        await asyncio.gather(*tasks)
 
 
-asyncio.run(main(LINK_TO_OXFORD_LIST))
+asyncio.run(main(settings.URL_TO_WORD_LIST))
