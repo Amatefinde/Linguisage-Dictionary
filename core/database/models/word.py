2bc1fad1b1a01eda8b80d65d65915c0bd5f05a7d
iRespectOnlyYen <MyNameIIIypuk@yandex.ru>
2023-11-30 18:04:42 +0300

    decompose models

--- core/database/models/word.py
+++ core/database/models/word.py
@@ -1,31 +1,10 @@
 from core.database import Base
+
 from sqlalchemy.orm import Mapped, mapped_column, relationship
-from sqlalchemy import ForeignKey
-from typing import Literal
+from typing import TYPE_CHECKING
 
-
-class Image(Base):
-    img: Mapped[str]
-
-    sense_id: Mapped[int] = mapped_column(ForeignKey("sense.id"))
-    sense: Mapped["Sense"] = relationship(back_populates="images")
-
-
-class Sense(Base):
-    lvl: Mapped[
-        Literal["A1", "A2", "B1", "B2", "C1", "C2"] | None
-    ] = mapped_column(default=None, server_default=None)
-    definition: Mapped[str]
-    short_cut: Mapped[str | None] = mapped_column(
-        default=None, server_default=None
-    )
-    examples: Mapped[list[str]] = mapped_column(default=[])
-    row_examples: Mapped[list[str]] = mapped_column(default=[])
-
-    images: Mapped[list["Image"]] = relationship(
-        back_populates="sense", cascade="all, delete-orphan"
-    )
-    word: Mapped["Word"] = relationship(back_populates="senses")
+if TYPE_CHECKING:
+    from .sense import Sense
 
 
 class Word(Base):
