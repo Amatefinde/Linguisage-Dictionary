88a90b9b9ddc39e067065f0065752de87bef980f
iRespectOnlyYen <MyNameIIIypuk@yandex.ru>
2023-11-30 13:42:34 +0300

    add parse_multiply and parse_single to package

--- Parsers/Dictionary/senses/__init__.py
+++ Parsers/Dictionary/senses/__init__.py
@@ -1,4 +1,9 @@
 from .sense_parser import Sense
+from .multiple import parse_multiply
+from .single import parse_single
 
-
-__all__ = [Sense]
+__all__ = [
+    Sense,
+    parse_multiply,
+    parse_single,
+]
