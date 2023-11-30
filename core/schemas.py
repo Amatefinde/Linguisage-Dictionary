4883f7e2c01bb999f19ab12c49a93a4c0f5f18ed
iRespectOnlyYen <MyNameIIIypuk@yandex.ru>
2023-11-30 13:55:10 +0300

    add images field to sense schema

--- core/schemas.py
+++ core/schemas.py
@@ -6,8 +6,9 @@ class SSense(BaseModel):
     lvl: Literal["A1", "A2", "B1", "B2", "C1", "C2"] | None = None
     definition: str
     short_cut: str | None = None
-    examples: list[str] | None = None
-    row_examples: list[str] | None = None
+    examples: list[str] | None = []
+    row_examples: list[str] | None = []
+    images: list[str] = []
 
     model_config = ConfigDict(from_attributes=True)
 
