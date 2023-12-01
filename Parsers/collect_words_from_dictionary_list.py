2671b13a278971dee2c4e55f733edadbab96485a
iRespectOnlyYen <MyNameIIIypuk@yandex.ru>
2023-12-01 20:38:52 +0300

    add split by batches

--- Parsers/collect_words_from_dictionary_list.py
+++ Parsers/collect_words_from_dictionary_list.py
@@ -1,11 +1,17 @@
 from typing import TypedDict
 import asyncio
 
+from aiohttp import TCPConnector
+
 from core.config import settings
 from Dictionary import get_dictionary_word_by_url
 
 import aiohttp
+from aiohttp.client import ClientTimeout
 from bs4 import BeautifulSoup
+
+from utils import split_on_batches
+from word_collector import get_word
 
 
 class Word(TypedDict):
@@ -16,6 +22,8 @@ class Word(TypedDict):
 headers = {
     "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/119.0.0.0 Safari/537.36 Edg/119.0.0.0"
 }
+
+session_timeout = ClientTimeout(6 * 60 * 60)
 
 
 async def fetch_data(session, url):
@@ -36,13 +44,23 @@ def get_links_to_words(row_html: str) -> list[Word]:
 
 
 async def main(link_to_list: str):
-    async with aiohttp.ClientSession(headers=headers) as session:
+    connector = TCPConnector(force_close=True)
+    async with aiohttp.ClientSession(
+        headers=headers,
+        timeout=session_timeout,
+        connector=connector,
+    ) as session:
         row_html = await fetch_data(session, link_to_list)
         words: list[Word] = get_links_to_words(row_html)
         tasks = []
         for word in words:  # type: Word
-            tasks.append(get_dictionary_word_by_url(session, word.get("link")))
-        print(await asyncio.gather(*tasks))
+            tasks.append(get_word(session, word.get("link")))
+        task_by_batches = split_on_batches(
+            tasks,
+        )
+        for batch in task_by_batches:
+            print(await asyncio.gather(*batch))
+    print("end")
 
 
 asyncio.run(main(settings.URL_TO_WORD_LIST))
