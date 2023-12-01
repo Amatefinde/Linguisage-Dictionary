498ed7da33ce0c257bac0a5727ef37e0ed1edf88
iRespectOnlyYen <MyNameIIIypuk@yandex.ru>
2023-12-01 20:40:55 +0300

    add api_v1 router

--- main.py
+++ main.py
@@ -1,9 +1,9 @@
 from fastapi import FastAPI
 from fastapi.staticfiles import StaticFiles
-
+from api_v1 import router as api_v1_router
 
 app = FastAPI()
+app.include_router(api_v1_router)
 
 
-app.mount("/static", StaticFiles(directory="app/static"), name="static")
-
+# app.mount("/static", StaticFiles(directory="app/static"), name="static")
