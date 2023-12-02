f59fdd4be09465e811d7830acbfdccaa9ff65f7f
iRespectOnlyYen <MyNameIIIypuk@yandex.ru>
2023-12-02 08:47:16 +0300

    initial commit

---
+++ Parsers/__init__.py
@@ -0,0 +1,3 @@
+from .word_collector import get_word
+
+__all__ = ["get_word"]
---
+++ utils/special/cut_word_list.py
@@ -0,0 +1,12 @@
+from core.schemas import SWordDictionaryLink
+
+
+def _cut_word_link_list(
+    word_links: list[SWordDictionaryLink],
+    start_word: str | None = None,
+) -> list[SWordDictionaryLink] | None:
+    if not start_word:
+        return
+    words = [word_link.word for word_link in word_links]
+    index_start_word = words.index(start_word)
+    return word_links[index_start_word:]
