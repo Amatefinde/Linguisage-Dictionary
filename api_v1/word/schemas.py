96abc36eb3a653b0bc122574bffaacff02636e9e
iRespectOnlyYen <MyNameIIIypuk@yandex.ru>
2023-12-02 22:23:06 +0300

    add DTO schemas

---
+++ api_v1/word/schemas.py
@@ -0,0 +1,36 @@
+from typing import Literal
+
+from pydantic import BaseModel, ConfigDict
+
+
+class BaseDTO(BaseModel):
+    model_config = ConfigDict(from_attributes=True)
+
+
+class ExamplesDTO(BaseDTO):
+    example: str
+
+
+class RowExamplesDTO(BaseDTO):
+    row_example: str
+
+
+class Image(BaseDTO):
+    id: int
+    img: str
+
+
+class SenseDTO(BaseDTO):
+    id: int
+    lvl: Literal["A1", "A2", "B1", "B2", "C1", "C2"] | None = None
+    short_cut: str | None = None
+    definition: str
+    examples: list[ExamplesDTO] = None
+    row_examples: list[RowExamplesDTO] = None
+    images: list[Image] = None
+
+
+class WordDTO(BaseDTO):
+    id: int
+    word: str
+    senses: list[SenseDTO]
