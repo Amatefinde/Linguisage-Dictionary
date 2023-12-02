859c68b9e65965ff746f0e1e858d1e04d3397f71
iRespectOnlyYen <MyNameIIIypuk@yandex.ru>
2023-12-02 14:21:52 +0300

    fixed return none

--- utils/special/cut_word_list.py
+++ utils/special/cut_word_list.py
@@ -4,9 +4,9 @@ from core.schemas import SWordDictionaryLink
 def _cut_word_link_list(
     word_links: list[SWordDictionaryLink],
     start_word: str | None = None,
-) -> list[SWordDictionaryLink] | None:
+) -> list[SWordDictionaryLink]:
     if not start_word:
-        return
+        return word_links
     words = [word_link.word for word_link in word_links]
     index_start_word = words.index(start_word)
     return word_links[index_start_word:]
