e0933924adceb9f7e3f04cd4a0d76550830facf2
iRespectOnlyYen <MyNameIIIypuk@yandex.ru>
2023-12-02 22:22:42 +0300

    script complete

--- Parsers/scripts/100_000_word/get_100_000_word.py
+++ Parsers/scripts/100_000_word/get_100_000_word.py
@@ -2,7 +2,12 @@ import asyncio
 
 import aiohttp
 from aiohttp import TCPConnector, ClientTimeout, ClientSession
+from sqlalchemy.ext.asyncio import AsyncSession
+
 from Parsers import get_url_by_query
+import api_v1
+from core.database import db_helper
+from utils import split_on_batches
 
 
 def clear_words(words: list[str]) -> list[str]:
@@ -11,7 +16,7 @@ def clear_words(words: list[str]) -> list[str]:
     return list(words)
 
 
-async def get_session() -> ClientSession:
+def get_session() -> ClientSession:
     aiohttp_session_config: dict = {
         "headers": {
             "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/119.0.0.0 Safari/537.36 Edg/119.0.0.0"
@@ -30,15 +35,24 @@ async def get_word_by_alias(session, alias: str) -> str:
     return word
 
 
+async def set_alias_to_word(
+    session: ClientSession,
+    alias: str,
+):
+    async with db_helper.session_factory() as db_session:
+        async with db_session.begin():
+            word = await get_word_by_alias(session, alias)
+            db_alias = await api_v1.word.crud.add_alias_to_word(db_session, alias, word)
+            print(word, db_alias)
+
+
 async def main():
     with open("wiki-100k.txt", "r", encoding="utf-8") as file:
         aliases = file.readlines()
         aliases = clear_words(aliases)
     async with get_session() as session:
-        for alias in aliases:
-            word = get_word_by_alias(session, alias)
-
-            # todo add alias to word in db
+        for alias_batch in split_on_batches(aliases, 30):
+            await asyncio.gather(*[set_alias_to_word(session, alias) for alias in alias_batch])
 
     print(aliases[:100], sep="\n")
 
