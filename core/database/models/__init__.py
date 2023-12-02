3c58b786ef66d79d40ba3dafc0ed33df92dc69dd
iRespectOnlyYen <MyNameIIIypuk@yandex.ru>
2023-12-02 16:32:00 +0300

    add alias table

--- core/database/models/__init__.py
+++ core/database/models/__init__.py
@@ -4,9 +4,10 @@ __all__ = (
     "Image",
     "RowExample",
     "Example",
+    "Alias",
 )
 
-from .word import Word
+from .word import Word, Alias
 from .sense import Sense, Image
 from .row_example import RowExample
 from .example import Example
