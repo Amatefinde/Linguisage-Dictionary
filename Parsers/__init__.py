48f6a0adb47b3553088bee54dd53b304f9a35378
iRespectOnlyYen <MyNameIIIypuk@yandex.ru>
2023-12-03 09:49:39 +0300

    -

--- Parsers/__init__.py
+++ Parsers/__init__.py
@@ -1,4 +1,3 @@
-from Parsers.word_collector import get_word
-from .dictionary_searcher import get_url_by_query
+from .word_collector import get_url_by_query
 
-__all__ = ["get_word"]
+__all__ = ["get_url_by_query"]
