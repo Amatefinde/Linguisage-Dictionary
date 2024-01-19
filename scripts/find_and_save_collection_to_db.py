5b5f4b06ba8045c37e69a6cd6b692b2d04c3fd60
AMDisPOWER <MyNameIIIyPuk@yandex.ru>
2024-01-19 15:44:32 +0300

    fix session

--- scripts/find_and_save_collection_to_db.py
+++ scripts/find_and_save_collection_to_db.py
@@ -27,33 +27,32 @@ async def get_aiohttp_session():
 async def find_and_save_to_db(
     query: str,
     collector: SeleniumBaseImgCollector,
-    aiohttp_session: ClientSession,
 ) -> None:
-    try:
-        core_word: CoreSWord = await collect_and_download_one(
-            aiohttp_session, query=query, collector=collector
-        )
-    except GetImageLinksError:
-        await asyncio.sleep(2)
-        logger.error(query)
-        core_word: CoreSWord = await collect_and_download_one(
-            aiohttp_session, query=query, collector=collector
-        )
-    logger.info(core_word)
-    async with db_helper.session_factory() as db_session:
-        async with db_session.begin():
-            await crud.create_or_supplement_db_public_word(db_session, core_word)
+    async with await get_aiohttp_session() as aiohttp_session:
+        try:
+            core_word: CoreSWord = await collect_and_download_one(
+                aiohttp_session, query=query, collector=collector
+            )
+        except GetImageLinksError:
+            await asyncio.sleep(2)
+            logger.error(query)
+            core_word: CoreSWord = await collect_and_download_one(
+                aiohttp_session, query=query, collector=collector
+            )
+        logger.info(core_word)
+        async with db_helper.session_factory() as db_session:
+            async with db_session.begin():
+                await crud.create_or_supplement_db_public_word(db_session, core_word)
 
 
 async def find_many_and_save_to_db(words: Iterable[str]) -> None:
     image_collector = SeleniumBaseImgCollector()
     with image_collector:
-        async with await get_aiohttp_session() as aiohttp_session:
-            for idx, word in enumerate(words, 1):
-                logger.info((idx, word))
-                try:
-                    await find_and_save_to_db(word, image_collector, aiohttp_session)
-                except Exception as Ex:
-                    logger.error(Ex)
-                    await asyncio.sleep(1)
-                    await find_and_save_to_db(word, image_collector, aiohttp_session)
+        for idx, word in enumerate(words, 1):
+            logger.info((idx, word))
+            try:
+                await find_and_save_to_db(word, image_collector)
+            except Exception as Ex:
+                logger.error(Ex)
+                await asyncio.sleep(1)
+                await find_and_save_to_db(word, image_collector)
