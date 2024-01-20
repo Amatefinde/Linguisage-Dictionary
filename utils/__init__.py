de4b77fbf2240640cd42e38334e3b65a886981c9
AMDisPOWER <MyNameIIIyPuk@yandex.ru>
2024-01-20 20:33:32 +0300

    update imports

--- utils/__init__.py
+++ utils/__init__.py
@@ -1,4 +1,10 @@
 from .join_url import join_url
 from .split_on_batches import split_on_batches
+from .timers import timer, async_timer
 
-__all__ = ["join_url", "split_on_batches"]
+__all__ = [
+    "join_url",
+    "split_on_batches",
+    "timer",
+    "async_timer",
+]
