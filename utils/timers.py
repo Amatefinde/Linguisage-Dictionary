4c8556b0c1db9c63d2688f1bc6c3004f34c6886f
AMDisPOWER <MyNameIIIyPuk@yandex.ru>
2024-01-21 11:18:36 +0300

    replace to more precision time estimate method

--- utils/timers.py
+++ utils/timers.py
@@ -1,12 +1,12 @@
-import time
+from timeit import default_timer
 from loguru import logger
 
 
 def async_timer(func):
     async def wrapper(*args, **kwargs):
-        start = time.time()
+        start = default_timer()
         result = await func(*args, **kwargs)
-        end = time.time()
+        end = default_timer()
         logger.info(f"Время выполнения функции {func.__name__}: {end - start} секунд")
         return result
 
@@ -15,9 +15,9 @@ def async_timer(func):
 
 def timer(func):
     def wrapper(*args, **kwargs):
-        start = time.time()
+        start = default_timer()
         result = func(*args, **kwargs)
-        end = time.time()
+        end = default_timer()
         logger.info(f"Время выполнения функции {func.__name__}: {end - start} секунд")
         return result
 
