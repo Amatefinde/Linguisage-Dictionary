b93642e68e51d13288573daf65fb04c4f71bbfc6
AMDisPOWER <MyNameIIIyPuk@yandex.ru>
2024-01-24 13:05:44 +0300

    update

--- scripts/_100_000_word/get_100_000_word.py
+++ scripts/_100_000_word/get_100_000_word.py
@@ -14,7 +14,7 @@ async def main():
     with open(path.join(current_dir, "wiki-100k.txt"), "r", encoding="utf-8") as file:
         aliases = file.readlines()
         aliases = clear_words(aliases)
-        await find_many_and_save_to_db(aliases, 8100)
+        await find_many_and_save_to_db(aliases, 8500)
 
 
 asyncio.run(main())
