eeb7f82d7344ee56680eb80683df28ff45eaafa2
AMDisPOWER <MyNameIIIyPuk@yandex.ru>
2024-01-25 17:18:40 +0300

    import updates

--- api_v1/__init__.py
+++ api_v1/__init__.py
@@ -2,9 +2,11 @@ from fastapi import APIRouter
 
 from .public import router as public_router
 from .personalize import router as personalize_router
+from .common import router as common_router
 
 __all__ = ["router"]
 
 router = APIRouter(prefix="/api/v1")
+router.include_router(common_router)
 router.include_router(public_router)
 router.include_router(personalize_router)
