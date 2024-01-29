5c7c0299d76a1be9b7aa963a82c8db1d9a4e0c86
AMDisPOWER <MyNameIIIyPuk@yandex.ru>
2024-01-29 17:50:05 +0300

    add validation for response

--- api_v1/public/views.py
+++ api_v1/public/views.py
@@ -3,11 +3,12 @@ from sqlalchemy.ext.asyncio import AsyncSession
 
 from core.database import db_helper
 from . import crud
+from .schemas import SResponse
 
 router = APIRouter(prefix="/public", tags=["Public"])
 
 
-@router.get("/")
+@router.get("/", response_model=SResponse)
 async def get_by_alias(
     query: str,
     session: AsyncSession = Depends(db_helper.session_dependency),
