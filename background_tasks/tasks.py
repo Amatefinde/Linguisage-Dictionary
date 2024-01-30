1df8123d0fd9889ad959369b14ca079241103b4c
AMDisPOWER <MyNameIIIyPuk@yandex.ru>
2024-01-30 12:03:35 +0300

    fix background adding to dictionary

--- background_tasks/tasks.py
+++ background_tasks/tasks.py
@@ -8,12 +8,14 @@ from scripts import find_and_save_to_db
 from Parsers import SeleniumImgCollector
 from Parsers.main_collector import WordNotExist
 
+loop = asyncio.get_event_loop()
+
 
 @celery.task
 def find_and_add_world(query: str):
     image_collector = SeleniumImgCollector()
     with image_collector:
         try:
-            asyncio.run(find_and_save_to_db(query, image_collector))
+            loop.run_until_complete(find_and_save_to_db(query, image_collector))
         except WordNotExist:
             logger.info(f'The word "{query}" is not found')
