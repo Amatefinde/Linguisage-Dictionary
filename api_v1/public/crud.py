e631ab20001ff1dae79836dc600c00dd51d50100
AMDisPOWER <MyNameIIIyPuk@yandex.ru>
2024-01-30 15:53:07 +0300

    add method for get random senses

--- api_v1/public/crud.py
+++ api_v1/public/crud.py
@@ -1,6 +1,8 @@
+from typing import Literal
+
 from sqlalchemy.orm import selectinload, joinedload
 from sqlalchemy.ext.asyncio import AsyncSession
-from sqlalchemy import select
+from sqlalchemy import select, func
 
 from core.database.models import Word, Example, Sense, Alias, WordImage
 from .schemas import SResponse
@@ -20,3 +22,26 @@ async def get_full_word(session: AsyncSession, alias: str):
     row_response = await session.execute(stmt)
     if response := row_response.scalar():
         return SResponse.model_validate(response, from_attributes=True)
+
+
+async def get_random_words(
+    session: AsyncSession,
+    amount: int,
+    lvl: list[Literal["A1", "A2", "B1", "B2", "C1", "C2"]] | None,
+):
+    stmt = (
+        select(Sense)
+        .where(Sense.is_public)
+        .options(
+            joinedload(Sense.word),
+            joinedload(Sense.word, Word.word_images),
+            joinedload(Sense.examples),
+        )
+        .order_by(func.random())
+        .limit(amount)
+    )
+    if lvl:
+        stmt = stmt.where(Sense.lvl.in_(lvl))
+    row_response = await session.execute(stmt)
+    if response := row_response.unique().scalars().all():
+        return response
--- api_v1/public/views.py
+++ api_v1/public/views.py
@@ -1,5 +1,8 @@
-from fastapi import APIRouter, Depends, HTTPException, status
+from typing import Literal, Annotated, Optional
+
+from fastapi import APIRouter, Depends, HTTPException, status, Query, Body
 from loguru import logger
+from pydantic import Field
 from sqlalchemy.ext.asyncio import AsyncSession
 
 from core.database import db_helper
@@ -11,7 +14,7 @@ from background_tasks.tasks import find_and_add_world
 router = APIRouter(prefix="/public", tags=["Public"])
 
 
-@router.get("/", response_model=SResponse)
+@router.get("/query", response_model=SResponse)
 async def get_by_alias(
     query: str,
     session: AsyncSession = Depends(db_helper.session_dependency),
@@ -21,3 +24,12 @@ async def get_by_alias(
         find_and_add_world.delay(query)
         raise HTTPException(status_code=status.HTTP_404_NOT_FOUND)
     return response
+
+
+@router.get("/random")
+async def get_random(
+    amount: Annotated[int, Query(ge=1, le=100)],
+    lvl: Annotated[list[Literal["A1", "A2", "B1", "B2", "C1", "C2"]], Query()] = None,
+    session: AsyncSession = Depends(db_helper.session_dependency),
+):
+    return await crud.get_random_words(session, amount, lvl)
