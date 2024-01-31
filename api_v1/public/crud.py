f0b4f9418edffe6e1f5949effbb4918278ab386c
AMDisPOWER <MyNameIIIyPuk@yandex.ru>
2024-01-31 12:49:59 +0300

    implement get random senses method

--- api_v1/public/crud.py
+++ api_v1/public/crud.py
@@ -1,47 +1,66 @@
 from typing import Literal
 
-from sqlalchemy.orm import selectinload, joinedload
+from loguru import logger
+from sqlalchemy.orm import selectinload, joinedload, contains_eager
 from sqlalchemy.ext.asyncio import AsyncSession
 from sqlalchemy import select, func
 
 from core.database.models import Word, Example, Sense, Alias, WordImage
-from .schemas import SResponse
+from core.types import level_type, main_pos
+from .schemas import (
+    SWordResponse,
+    SRandomSensesResponse,
+    SPickedRandomSense,
+    SAdditionalRandomSense,
+)
 
 
 async def get_full_word(session: AsyncSession, alias: str):
     stmt = (
-        select(Alias)
-        .where(Alias.alias == alias)
+        select(Word)
         .options(
-            joinedload(Alias.word),
-            joinedload(Alias.word, Word.word_images),
-            joinedload(Alias.word, Word.senses.and_(Sense.is_public)),
-            joinedload(Alias.word, Word.senses, Sense.examples),
+            selectinload(Word.aliases),
+            selectinload(Word.word_images),
+            selectinload(Word.senses.and_(Sense.is_public)),
+            selectinload(Word.senses, Sense.examples),
         )
+        .join(Alias)
+        .where(Alias.alias == alias)
     )
     row_response = await session.execute(stmt)
     if response := row_response.scalar():
-        return SResponse.model_validate(response, from_attributes=True)
+        return SWordResponse.model_validate(response, from_attributes=True)
 
 
-async def get_random_words(
+async def get_random_senses(
     session: AsyncSession,
     amount: int,
-    lvl: list[Literal["A1", "A2", "B1", "B2", "C1", "C2"]] | None,
-):
+    lvl: list[level_type] | None = None,
+    pos: list[main_pos] | None = None,
+    black_list_sense_ids: list[int] | None = None,
+    load_examples_and_images: bool = False,
+) -> list[SPickedRandomSense] | list[SAdditionalRandomSense]:
     stmt = (
         select(Sense)
         .where(Sense.is_public)
-        .options(
-            joinedload(Sense.word),
-            joinedload(Sense.word, Word.word_images),
-            joinedload(Sense.examples),
-        )
+        .options(joinedload(Sense.word))
         .order_by(func.random())
         .limit(amount)
     )
+    if load_examples_and_images:
+        stmt = stmt.options(
+            selectinload(Sense.examples),
+            selectinload(Sense.word, Word.word_images),
+        )
     if lvl:
         stmt = stmt.where(Sense.lvl.in_(lvl))
+    if pos:
+        stmt = stmt.where(Sense.part_of_speech.in_(pos))
+    if black_list_sense_ids:
+        stmt = stmt.where(Sense.id.notin_(black_list_sense_ids))
     row_response = await session.execute(stmt)
-    if response := row_response.unique().scalars().all():
-        return response
+    db_senses = row_response.scalars().all()
+    if load_examples_and_images:
+        return [SPickedRandomSense.model_validate(sense) for sense in db_senses]
+    else:
+        return [SAdditionalRandomSense.model_validate(sense) for sense in db_senses]
