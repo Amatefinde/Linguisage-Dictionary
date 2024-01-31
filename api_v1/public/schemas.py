3085c6d6bceb055c072bac14026f917cb1cc8de9
AMDisPOWER <MyNameIIIyPuk@yandex.ru>
2024-01-31 12:49:45 +0300

    validation for get random senses method

--- api_v1/public/schemas.py
+++ api_v1/public/schemas.py
@@ -1,11 +1,16 @@
 from os.path import join
 from typing import List, Optional, Literal
-from pydantic import BaseModel, Field, validator, field_validator
+from pydantic import BaseModel, Field, field_validator, ConfigDict
 
 from core import settings
+from core.types import level_type
 
 
-class WordImage(BaseModel):
+class BaseWithConf(BaseModel):
+    model_config = ConfigDict(from_attributes=True)
+
+
+class WordImage(BaseWithConf):
     is_public: bool
     word_id: int = Field(exclude=True)
     id: int
@@ -18,35 +23,88 @@ class WordImage(BaseModel):
         return f"{domain}/{join('static','word_images', img)}"
 
 
-class Example(BaseModel):
+class Example(BaseWithConf):
+    id: int
+    example: str
     html_example: Optional[str]
     sense_id: int = Field(exclude=True)
-    example: str
-    id: int
 
 
-class Sense(BaseModel):
-    lvl: Literal["A1", "A2", "B1", "B2", "C1", "C2"] | None
-    short_cut: str | None = None
-    word_id: int = Field(exclude=True)
+class Sense(BaseWithConf):
     is_public: bool
     definition: str
     part_of_speech: str | None = None
     id: int
-    examples: List[Example]
+    lvl: level_type | None = None
+    short_cut: str | None = None
+    word_id: int = Field(exclude=True)
+    examples: List[Example] | None = None
 
 
-class Word(BaseModel):
+class Alias(BaseWithConf):
+    alias: str
+    word_id: int
+    id: int
+
+
+class SWordResponse(BaseWithConf):
     word: str
     id: int
-    sound_uk: str
-    sound_us: str
+    sound_uk: str | None = None
+    sound_us: str | None = None
     word_images: List[WordImage]
     senses: List[Sense]
+    aliases: List[Alias] = Field(exclude=True)
 
 
-class SResponse(BaseModel):
+########################
+
+
+class Word(BaseWithConf):
+    word: str
+    sound_us: str | None = None
+    sound_uk: str | None = None
+    id: int
+    word_images: List[WordImage] | None = None
+
+
+class SPickedRandomSense(BaseWithConf):
+    is_public: bool
+    definition: str
+    part_of_speech: str | None = None
+    id: int
+    lvl: level_type | None = None
+    short_cut: str | None = None
     word_id: int = Field(exclude=True)
-    id: int = Field(exclude=True)
-    alias: str
     word: Word
+    examples: List[Example] | None = Field()
+
+
+class SAdditionalRandomWord(BaseWithConf):
+    word: str
+    sound_us: str | None = None
+    sound_uk: str | None = None
+    id: int
+
+
+class SAdditionalRandomSense(BaseWithConf):
+    is_public: bool
+    definition: str
+    part_of_speech: str | None = None
+    id: int
+    lvl: level_type | None = None
+    short_cut: str | None = Field(default=None, exclude=True)
+    word_id: int = Field(exclude=True)
+    word: SAdditionalRandomWord
+
+
+class SAdditionalPOSs(BaseWithConf):
+    noun: list[SAdditionalRandomSense] = Field(default_factory=list)
+    verb: list[SAdditionalRandomSense] = Field(default_factory=list)
+    adjective: list[SAdditionalRandomSense] = Field(default_factory=list)
+    adverb: list[SAdditionalRandomSense] = Field(default_factory=list)
+
+
+class SRandomSensesResponse(BaseWithConf):
+    senses: list[SPickedRandomSense]
+    additional: SAdditionalPOSs
