5c8213d6502f8462bf107dcaf33ea4b7a4ea4f3d
Alexandr Gurov <MyNameIIIypuk@yandex.ru>
2024-02-15 16:50:06 +0300

    test continues delivery

--- main.py
+++ main.py
@@ -16,7 +16,7 @@ from admin import (
 
 make_static_folder()
 
-app = FastAPI(title="Linguisage Dictionary!")
+app = FastAPI(title="Linguisage Dictionary!!")
 app.include_router(api_v1_router)
 
 app.mount(
