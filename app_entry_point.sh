c702a836da1e82a63da288b14f0c9f9664057c40
Alexandr Gurov <MyNameIIIypuk@yandex.ru>
2024-03-16 16:19:59 +0300

    add ssl cert for docker pipeline

--- app_entry_point.sh
+++ app_entry_point.sh
@@ -2,4 +2,4 @@
 
 alembic upgrade head
 
-uvicorn main:app --host 0.0.0.0 --port 9100 --ssl-certfile=./cert/fullchain.pem --ssl-keyfile=./cert/privkey.pem
+uvicorn main:app --host 0.0.0.0 --port 9100
