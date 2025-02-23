abcaf09cd4711acd20edb3fab95bbf6622b46e4d
Amatefinde <gurov@tm-serv.ru>
2025-02-23 17:36:38 +0300

    ready to deploy

--- background_tasks/tasks.py
+++ background_tasks/tasks.py
@@ -5,7 +5,6 @@ from loguru import logger
 
 from .celery import celery
 from scripts import find_and_save_to_db
-from Parsers import SeleniumImgCollector
 from Parsers.main_collector import WordNotExist
 
 loop = asyncio.get_event_loop()
@@ -13,9 +12,7 @@ loop = asyncio.get_event_loop()
 
 @celery.task
 def find_and_add_world(query: str):
-    image_collector = SeleniumImgCollector()
-    with image_collector:
-        try:
-            loop.run_until_complete(find_and_save_to_db(query, image_collector))
-        except WordNotExist:
-            logger.info(f'The word "{query}" is not found')
+    try:
+        loop.run_until_complete(find_and_save_to_db(query))
+    except WordNotExist:
+        logger.info(f'The word "{query}" is not found')
